4e049b79e32e937c5a98d022d5418647bf2f0e96
Aidan Torrence <aidan.torrence@gmail.com>
2022-01-18 23:27:09 -0500

    add reader styling

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
-import './App.css'
+import { motion } from 'framer-motion';
+import './App.css';
 
 export default function App() {
-  const [inputValue, setInputValue] = useState(`set up electron app <=> set up nextJS portfolio <=> reddit board that shows top to-dos linked to their site of choice (rewards good to do ers with a link to their website of choice)`)
-  return <div className="main">
-    <textarea className="text-area" 
-      onChange={e => setInputValue(e.target.value)}
-      value={inputValue}
-      spellcheck="false"
-    />
-
-    </div>
+  return (
+    <>
+      <div className="main">
+        <div className="text">alkjfklakdfalkfl</div>
+        <motion.div
+          className="background"
+          key={undefined}
+          initial={{ scale: 1.03 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.1 }}
+        />
+      </div>
+      <div className="overlay" />
+    </>
+  );
 }
